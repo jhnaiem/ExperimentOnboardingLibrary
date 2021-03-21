bd08c9cac5d2f69c4ef507d2f44bcef9ae5384cc
Jahid Hasan Naiem <naiem@field.buzz>
2021-03-21 12:54:12 +0600

    Implemeneted showcaseview in fragment

--- app/src/main/java/com/example/onboardingnavdrawer/view/MenuFragment.java
+++ app/src/main/java/com/example/onboardingnavdrawer/view/MenuFragment.java
@@ -1,24 +1,51 @@
 package com.example.onboardingnavdrawer.view;
 
+import android.graphics.Point;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import com.example.onboardingnavdrawer.R;
+import com.getkeepsafe.taptargetview.TapTarget;
+import com.getkeepsafe.taptargetview.TapTargetView;
+import com.github.amlcurran.showcaseview.ShowcaseView;
+import com.github.amlcurran.showcaseview.targets.Target;
+import com.github.amlcurran.showcaseview.targets.ViewTarget;
 
 public class MenuFragment extends Fragment {
+
+    private ShowcaseView showcaseView;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
         container.removeAllViews();
-        View view = inflater.inflate(R.layout.fragment_menu,container,false);
+        View view = inflater.inflate(R.layout.fragment_menu, container, false);
+
+        Target target = new Target() {
+            @Override
+            public Point getPoint() {
+                return new Point(700, 1400);
+            }
+        };
+
+        showcaseView = new ShowcaseView.Builder(getActivity())
+                .setTarget(new ViewTarget(view.findViewById(R.id.button1)))
+                .setContentTitle("This is title").hideOnTouchOutside()
+                .setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Toast.makeText(getContext(),"Clicked",Toast.LENGTH_LONG);
+                    }
+                })
+                .build();
         return view;
     }
 
